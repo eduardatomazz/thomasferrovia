760b71f757455ad3f33d668e9e29f9e388e7fa8d
Larissa <larissa_boing@estudante.sesisenai.org.br>
2025-09-18 11:11:44 -0300

    add - notmotorista em php

---
+++ scripts/notmotorista.php
@@ -0,0 +1,12 @@
+<?php
+function enviarMensagem() {
+    $mensagem = trim($_POST['mensagem']);
+
+    if ($mensagem === "") {
+        echo "<script>alert('Por favor, escreva uma mensagem antes de enviar.');</script>";
+    } else {
+        echo "<script>alert('Mensagem enviada ao motorista:\\n\\n" . addslashes($mensagem) . "');</script>";
+        $_POST['mensagem'] = "";
+    }
+}
+?>
