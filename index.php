c5bc37340dfa39ba396a7f480f64c820023929d8
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-09-25 11:01:08 -0300

    add- arrumei o style

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thomas_ferrovia</title>
-    <link rel ="stylesheet" href="styles/style.css">
+    <link rel ="stylesheet" href="style.css">
     <script src="../php/script.php"> </script>
     <script src="../php/eqmanutencao.php"> </script>
     <script src="../php/login.php"> </script>
